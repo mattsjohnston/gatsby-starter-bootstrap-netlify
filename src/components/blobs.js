3f87af338c7161af1cf1f071cb8340bd191a94f5
Matt Johnston <matt@tallpixels.com>
2019-02-25 16:48:08 -0800

    Adding more responsive rules for blob animation

--- src/components/blobs.js
+++ src/components/blobs.js
@@ -3,27 +3,28 @@ import React from 'react'
 
 const Blobs = () => {
   return (
-    <div class="blobs-container">
-      <div class='blobs blobs-blue'>
-        <div class="blob"></div>
-        <div class="blob"></div>
-        <div class="blob"></div>
+    <div className="blobs-container">
+      <div className='blobs blobs-blue'>
+        <div className="blob"></div>
+        <div className="blob"></div>
+        <div className="blob"></div>
       </div>
-      <div class='blobs blobs-yellow'>
-        <div class="blob"></div>
-        <div class="blob"></div>
+      <div className='blobs blobs-yellow'>
+        <div className="blob"></div>
+        <div className="blob"></div>
       </div>
-      <div class='blobs blobs-subtle'>
-        <div class="blob"></div>
-        <div class="blob"></div>
-        <div class="blob"></div>
-        <div class="blob"></div>
+      <div className='blobs blobs-subtle'>
+        <div className="blob"></div>
+        <div className="blob"></div>
+        <div className="blob"></div>
+        <div className="blob"></div>
       </div>
-      <div class='blobs blobs-white'>
-        <div class="blob"></div>
+      <div className='blobs blobs-white'>
+        <div className="blob"></div>
+        <div className="blob"></div>
       </div>
-      <div class='blobs blobs-orange'>
-        <div class="blob"></div>
+      <div className='blobs blobs-orange'>
+        <div className="blob"></div>
       </div>
 
       <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
@@ -36,13 +37,13 @@ const Blobs = () => {
 
           <filter id="goo-md">
             <feGaussianBlur in="SourceGraphic" stdDeviation="60" result="blur-md" />
-            <feColorMatrix in="blur-md" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 660 -209" />
+            <feColorMatrix in="blur-md" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 480 -152" />
             <feBlend in2="goo-md" in="SourceGraphic" result="mix" />
           </filter>
 
           <filter id="goo-lg">
-            <feGaussianBlur in="SourceGraphic" stdDeviation="100" result="blur" />
-            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 600 -190" />
+            <feGaussianBlur in="SourceGraphic" stdDeviation="60" result="blur" />
+            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 300 -70" />
             <feBlend in2="goo-lg" in="SourceGraphic" result="mix" />
           </filter>
         </defs>
